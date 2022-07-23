e073c4b80d26f75146decb48d84a96574e751e86
Ismail Chbiki <ismailchbiki.nl@gmail.com>
2022-07-23 22:54:16 +0200

    System Icons & SF Symboles

---
+++ SwiftUI Bootcamp/SystemIcons.swift
@@ -0,0 +1,31 @@
+//
+//  System Icons.swift
+//  SwiftUI Bootcamp
+//
+//  Created by Ismail on 23/07/2022.
+//
+
+import SwiftUI
+
+struct System_Icons: View {
+    var body: some View {
+                Image(systemName: "person.fill.badge.plus")
+            .renderingMode(.original)
+            .font(.largeTitle)
+//            .resizable()
+//            .aspectRatio(contentMode: .fit)
+//            .scaledToFit()
+//            .scaledToFill()
+//            .font(.caption)
+//            .font(.system(size: 200))
+//            .foregroundColor(.orange)
+//            .frame(width: 300, height: 300)
+//            .clipped()
+    }
+}
+
+struct System_Icons_Previews: PreviewProvider {
+    static var previews: some View {
+        System_Icons()
+    }
+}
