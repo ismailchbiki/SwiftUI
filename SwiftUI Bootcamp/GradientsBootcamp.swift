35e8a639633abf783336a445824f7e13d13c1730
Ismail Chbiki <ismailchbiki.nl@gmail.com>
2022-07-23 21:11:04 +0200

    Color gradients

---
+++ SwiftUI Bootcamp/GradientsBootcamp.swift
@@ -0,0 +1,39 @@
+//
+//  GradiantsBootcamp.swift
+//  SwiftUI Bootcamp
+//
+//  Created by Ismail on 23/07/2022.
+//
+
+import SwiftUI
+
+struct GradiantsBootcamp: View {
+    var body: some View {
+        RoundedRectangle(cornerRadius: 20)
+            .fill(
+//                Color("CustomColor")
+//                Color.red
+//                LinearGradient(
+//                    gradient: Gradient(colors: [.blue, .red, .yellow, .green]),
+//                    startPoint: .topLeading,
+//                    endPoint: .bottomTrailing)
+//                RadialGradient(
+//                    colors: [.red, .blue],
+//                    center: .topLeading,
+//                    startRadius: 5,
+//                    endRadius: 400)
+                AngularGradient(
+                    gradient: Gradient(colors: [.red, .blue]),
+                    center: .topLeading,
+                    angle: .degrees(180 + 45))
+            )
+            .frame(width: 300, height: 200)
+    }
+}
+
+struct GradiantsBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        GradiantsBootcamp()
+            
+    }
+}
